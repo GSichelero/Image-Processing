525429a2f9829aeb1281302d9399f9c0591b0964
GSichelero <gabriel.sichelero@hotmail.com>
2024-04-02 01:45:36 -0300

    add separated filter

--- mean-filter.py
+++ mean-filter.py
@@ -41,6 +41,39 @@ def naive_mean_filter(img, window_size):
     return output_img
 
 
+def separated_mean_filter(img, window_size):
+    height = img.shape[0]
+    width = img.shape[1]
+    color_channels = img.shape[2]
+    half_window = window_size // 2
+    horizontally_filtered_img = np.zeros((height, width, color_channels), np.uint8)
+    output_img = np.zeros((height, width, color_channels), np.uint8)
+
+    for h in range(color_channels):
+        for i in range(height):
+            for j in range(width):
+                sum_values = 0
+                count_pixels = 0
+                for k in range(-half_window, half_window + 1):
+                    if j+k >= 0 and j+k < width:
+                        sum_values += img[i, j+k, h]
+                        count_pixels += 1
+                horizontally_filtered_img[i, j, h] = sum_values // count_pixels
+
+    for h in range(color_channels):
+        for i in range(height):
+            for j in range(width):
+                sum_values = 0
+                count_pixels = 0
+                for k in range(-half_window, half_window + 1):
+                    if i+k >= 0 and i+k < height:
+                        sum_values += horizontally_filtered_img[i+k, j, h]
+                        count_pixels += 1
+                output_img[i, j, h] = sum_values // count_pixels
+
+    return output_img
+
+
 
 def main ():
     img = cv2.imread(INPUT_IMAGE)
@@ -55,6 +88,11 @@ def main ():
     print('Tempo Algoritmo “ingênuo”: %f' % (timeit.default_timer() - start_time))
     cv2.imshow('Imagem filtrada Algoritmo “ingênuo”', output_img)
 
+    start_time = timeit.default_timer()
+    output_img = separated_mean_filter(img, WINDOW)
+    print('Tempo Filtro separável: %f' % (timeit.default_timer() - start_time))
+    cv2.imshow('Imagem filtrada Filtro separável', output_img)
+    
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
