78e4c9ed0f9f1e7e163e7ab632375fefe66f549b
GSichelero <gabriel.sichelero@hotmail.com>
2024-04-02 02:59:48 -0300

    integral image filter

--- mean-filter.py
+++ mean-filter.py
@@ -74,6 +74,47 @@ def separated_mean_filter(img, window_size):
     return output_img
 
 
+def integral_image_filter(img, window_size):
+    def calculate_integral_image(img):
+        height = img.shape[0]
+        width = img.shape[1]
+        color_channels = img.shape[2]
+        integral_img = np.zeros((height, width, color_channels), np.uint32)
+        for h in range(color_channels):
+            for i in range(height):
+                for j in range(width):
+                    integral_img[i, j, h] = img[i, j, h]
+                    if i > 0:
+                        integral_img[i, j, h] += integral_img[i-1, j, h]
+                    if j > 0:
+                        integral_img[i, j, h] += integral_img[i, j-1, h]
+                    if i > 0 and j > 0:
+                        integral_img[i, j, h] -= integral_img[i-1, j-1, h]
+
+        return integral_img
+    
+    
+    height = img.shape[0]
+    width = img.shape[1]
+    color_channels = img.shape[2]
+    half_window = window_size // 2
+    integral_img = calculate_integral_image(img)
+    output_img = np.zeros((height, width, color_channels), np.uint8)
+
+    for h in range(color_channels):
+        for i in range(height):
+            for j in range(width):
+                x1 = max(0, i - half_window)
+                x2 = min(height - 1, i + half_window)
+                y1 = max(0, j - half_window)
+                y2 = min(width - 1, j + half_window)
+                count_pixels = (x2 - x1 + 1) * (y2 - y1 + 1)
+                sum_values = integral_img[x2, y2, h] + integral_img[x1, y1, h] - integral_img[x1, y2, h] - integral_img[x2, y1, h]
+                output_img[i, j, h] = sum_values // count_pixels
+
+    return output_img
+    
+
 
 def main ():
     img = cv2.imread(INPUT_IMAGE)
@@ -92,7 +133,12 @@ def main ():
     output_img = separated_mean_filter(img, WINDOW)
     print('Tempo Filtro separável: %f' % (timeit.default_timer() - start_time))
     cv2.imshow('Imagem filtrada Filtro separável', output_img)
-    
+
+    start_time = timeit.default_timer()
+    output_img = integral_image_filter(img, WINDOW)
+    print('Tempo Algoritmo com imagens integrais: %f' % (timeit.default_timer() - start_time))
+    cv2.imshow('Imagem filtrada Algoritmo com imagens integrais', output_img)
+
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
